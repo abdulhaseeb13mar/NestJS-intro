578eac69cb1fb17465096aab0975c355409a8af8
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2021-06-09 17:55:26 +0500

    CRUD operations completed with mongoDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://abdulhaseeb:mongodb123@cluster0.duzab.mongodb.net/nestjs-demo?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
